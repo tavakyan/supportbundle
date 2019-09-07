85429deaa4eee748faf1b9a28eb15c5b62e183be
Tigran Avakyan <tavakyan@Tigrans-MacBook-Pro.local>
2019-09-06 20:09:38 -0700

    Added impl for docker and commands

--- commands.go
+++ commands.go
@@ -1,14 +1,56 @@
 package supportbundle
 
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+const LoadAvgPath = "/loadavg/loadavg"
+const CommandPath = "/commands"
+
 type LoadAvg struct {
 	LoadTime15m string
+}
+
+// Converts file containing:
+// 0.26 0.14 0.05 5/233 5186
+// to LoadAvg structure, error otherwise
+func (avg LoadAvg) unmarshal(path string) error {
+
+	// Set path to LoadAvg command
+	// an load file from that path
+	p := path + LoadAvgPath
+	f, err := os.Open(p)
+	defer f.Close()
+
+	if err != nil {
+		return err
+	}
+
+	// Start reading from the file with a reader.
+	reader := bufio.NewReader(f)
+	line, err := reader.ReadString('\n')
+
+	// Delimit by spaces
+	res := strings.Split(line, " ")
+
+	// Set LoadAvg params
+	avg.LoadTime15m = res[2]
+
+	return nil
 }
 
 type Commands struct {
 	LoadAvg LoadAvg
 }
 
-func (commands Commands) Unmarshal(path string) error {
+func (commands Commands) Unmarshal(path string) (err error) {
+
+	err = commands.LoadAvg.unmarshal(path + CommandPath)
+	if err != nil {
+		return err
+	}
 
 	return nil
 
--- docker.go
+++ docker.go
@@ -1,10 +1,55 @@
 package supportbundle
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+const DockerVersionPath = "/docker_version.json"
+const DockerInfoPath = "/docker_info.json"
+const DockerPath = "/docker"
+
+type DockerVersion struct {
+	Version string
+}
+
+func (dv DockerVersion) unmarshal(path string) error {
+
+	// Set path to LoadAvg command
+	// an load file from that path
+	p := path + DockerVersionPath
+	res, err := ioutil.ReadFile(p)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(res, dv)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 type DockerInfo struct {
 	Driver string
 }
-type DockerVersion struct {
-	Version string
+
+func (di DockerInfo) unmarshal(path string) error {
+	// Set path to LoadAvg command
+	// an load file from that path
+	p := path + DockerInfoPath
+	res, err := ioutil.ReadFile(p)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(res, di)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 type Docker struct {
@@ -12,6 +57,17 @@ type Docker struct {
 	DockerInfo    DockerInfo
 }
 
-func (docker Docker) Unmarshal(path string) error {
+func (docker Docker) Unmarshal(path string) (err error) {
+
+	err = docker.DockerVersion.unmarshal(path + DockerPath)
+	if err != nil {
+		return err
+	}
+
+	err = docker.DockerInfo.unmarshal(path + DockerPath)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
