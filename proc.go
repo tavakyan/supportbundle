7530d26b1480e159d570bd71e29daa890bb8f24d
Tigran Avakyan <tavakyan@Tigrans-MacBook-Pro.local>
2019-09-06 20:21:06 -0700

    Implemented some of the proc module

--- proc.go
+++ proc.go
@@ -1,8 +1,23 @@
 package supportbundle
 
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+const OSPath = "/version"
+const CPUInfoPath = "/cpuinfo"
+const MemInfoPath = "/meminfo"
+const ProcPath = "/proc"
+
 type OS struct {
 	Name    string
 	Version string
+}
+
+func (os OS) unmarshal(path string) error {
+	return nil
 }
 
 type MemInfo struct {
@@ -10,8 +25,50 @@ type MemInfo struct {
 	MemAvailable string
 }
 
+func (memInfo MemInfo) unmarshal(path string) error {
+
+	p := path + MemInfoPath
+	f, err := os.Open(p)
+	defer f.Close()
+
+	if err != nil {
+		return err
+	}
+
+	// Start reading from the file with a reader.
+	reader := bufio.NewReader(f)
+
+	var line string
+	var parsed map[string]string
+
+	for {
+		line, err = reader.ReadString('\n')
+
+		if err != nil {
+			break
+		}
+
+		line = strings.TrimSpace(line)
+
+		res := strings.Split(line, ":")
+
+		k, v := res[0], res[1]
+
+		parsed[k] = v
+	}
+
+	memInfo.MemAvailable = parsed["MemAvailable"]
+	memInfo.MemTotal = parsed["MemTotal"]
+
+	return nil
+}
+
 type CpuInfo struct {
 	CpuCores string
+}
+
+func (ci CpuInfo) unmarshal(path string) error {
+	return nil
 }
 
 type Proc struct {
@@ -20,7 +77,22 @@ type Proc struct {
 	OS      OS
 }
 
-func (proc Proc) Unmarshal(path string) error {
+func (proc Proc) Unmarshal(path string) (err error) {
+
+	err = proc.MemInfo.unmarshal(path + ProcPath)
+	if err != nil {
+		return err
+	}
+
+	err = proc.CpuInfo.unmarshal(path + ProcPath)
+	if err != nil {
+		return err
+	}
+
+	err = proc.OS.unmarshal(path + ProcPath)
+	if err != nil {
+		return err
+	}
 
 	return nil
 
